f28d581003aed35ea7fbc377b8bf3f8c39ed2e20
tulu145 <dedebnathtulanka@gmail.com>
2026-08-27 22:49:51 +0530

    Remove Herosection component from Home page as requested

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, lazy, Suspense } from "react";
 import { Layout } from "../components/common/Layout/Layout";
-import { Herosection } from "../components/Homepage/Herosection/Herosection";
 import { SortProduct } from "../components/Homepage/ShortProducts/SortProduct";
 import { Productsection } from "../components/Homepage/ProductsSection/Productsection";
 import { Footer } from "../components/common/Footer/Footer";
@@ -15,8 +14,6 @@ export const Home = () => {
   }, []);
   return (
     <Layout>
-      <Herosection />
-      
       <SortProduct />
      
       <Suspense fallback={<div style={{ minHeight: '200px' }} />}>
